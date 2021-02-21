8a6c32002c44d92ac8da49ebd7ddd21486384f2b
nguyentugiang2411 <49792268+nguyentugiang2411@users.noreply.github.com>
2021-02-21 19:44:12 +0700

    init

--- deno/routes.ts
+++ deno/routes.ts
@@ -8,7 +8,7 @@ const   customers   = JSON.parse(loadFile('../data/customers.json')),
 router.get<{skip: string, top: string}>('/api/customers/page/:skip/:top', (ctx) => {
     const topVal = +ctx.params.top,
           skipVal = +ctx.params.skip,
-          skip = (isNaN(skipVal)) ? 0 : skipVal;  
+          skip = (isNaN(skipVal)) ? 0 : skipVal;
     let top = (isNaN(topVal)) ? 10 : skip + (topVal);
 
     if (top > customers.length) {
@@ -17,7 +17,7 @@ router.get<{skip: string, top: string}>('/api/customers/page/:skip/:top', (ctx) 
 
     console.log(`Skip: ${skip} Top: ${top}`);
 
-    var pagedCustomers = customers.slice(skip, top);
+    const pagedCustomers = customers.slice(skip, top);
     ctx.response.headers.set('X-InlineCount', customers.length);
     ctx.response.body = pagedCustomers;
 });
@@ -27,22 +27,22 @@ router.get('/api/customers', (ctx) => {
 });
 
 router.get<{id: string}>('/api/customers/:id', (ctx) => {
-    let customerId = +ctx.params.id;
+    const customerId = +ctx.params.id;
     let selectedCustomer = null;
-    for (let customer of customers) {
+    for (const customer of customers) {
         if (customer.id === customerId) {
            // found customer to create one to send
            selectedCustomer = {};
            selectedCustomer = customer;
            break;
         }
-    }  
+    }
     ctx.response.body = selectedCustomer;
 });
 
 router.post('/api/customers', async (ctx) => {
-    let postedCustomer = (await ctx.request.body()).value;
-    let maxId = Math.max.apply(Math,customers.map((cust: any) => cust.id));
+    const postedCustomer = (Object) (await ctx.request.body()).value;
+    let maxId = Math.max.apply(Math, customers.map((cust: any) => cust.id));
     postedCustomer.id = ++maxId;
     postedCustomer.gender = (postedCustomer.id % 2 === 0) ? 'female' : 'male';
     customers.push(postedCustomer);
@@ -50,19 +50,19 @@ router.post('/api/customers', async (ctx) => {
 });
 
 router.put<{id: string}>('/api/customers/:id', async (ctx) => {
-    let putCustomer = (await ctx.request.body()).value;
+    const putCustomer = (Object) (await ctx.request.body()).value;
     console.log(putCustomer);
-    let id = +ctx.params.id;
+    const id = +ctx.params.id;
     let status = false;
 
-    //Ensure state name is in sync with state abbreviation 
+    // Ensure state name is in sync with state abbreviation
     const filteredStates = states.filter((state: any) => state.abbreviation === putCustomer.state.abbreviation);
     if (filteredStates && filteredStates.length) {
         putCustomer.state.name = filteredStates[0].name;
         console.log('Updated putCustomer state to ' + putCustomer.state.name);
     }
 
-    for (let i=0,len=customers.length;i<len;i++) {
+    for (let i = 0, len = customers.length; i < len; i++) {
         if (customers[i].id === id) {
             customers[i] = putCustomer;
             status = true;
@@ -73,19 +73,19 @@ router.put<{id: string}>('/api/customers/:id', async (ctx) => {
 });
 
 router.delete<{id: string}>('/api/customers/:id', (ctx) => {
-    let customerId = +ctx.params.id;
-    for (let i=0,len=customers.length;i<len;i++) {
+    const customerId = +ctx.params.id;
+    for (let i = 0, len = customers.length; i < len; i++) {
         if (customers[i].id === customerId) {
-           customers.splice(i,1);
+           customers.splice(i, 1);
            break;
         }
-    }  
+    }
     ctx.response.body = { status: true };
 });
 
 router.get<{id: string}>('/api/orders/:id', (ctx) => {
-    let customerId = +ctx.params.id;
-    for (let cust of customers) {
+    const customerId = +ctx.params.id;
+    for (const cust of customers) {
         if (cust.customerId === customerId) {
             return ctx.response.body = cust;
         }
@@ -98,8 +98,8 @@ router.get('/api/states', (ctx) => {
 });
 
 router.post('/api/auth/login', async (ctx) => {
-    var userLogin = (await ctx.request.body()).value;
-    //Add "real" auth here. Simulating it by returning a simple boolean.
+    const userLogin = (await ctx.request.body()).value;
+    // Add "real" auth here. Simulating it by returning a simple boolean.
     ctx.response.body = true;
 });
 
